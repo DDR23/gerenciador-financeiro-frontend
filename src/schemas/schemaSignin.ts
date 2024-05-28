dfcd5ddff346b6f0240a59486bb36b21e33c79ca
DDR23 <roboreserva.andre@gmail.com>
2024-05-27 22:19:09 -0300

    fix:{adjustment on validator schemaSignin}

--- src/schemas/schemaSignin.ts
+++ src/schemas/schemaSignin.ts
@@ -3,7 +3,9 @@ import * as yup from 'yup'
 export const schemaSignin = yup.object().shape({
   USER_EMAIL: yup
     .string()
-    .email('Invalid email'),
-  USER_PASSWORD: yup
+    .email('Invalid email')
+    .required('Required'),
+    USER_PASSWORD: yup
     .string()
+    .required('Required')
 });
