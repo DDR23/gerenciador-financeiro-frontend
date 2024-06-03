3583a602b51508117eba47d262612e51e32a65c7
DDR23 <roboreserva.andre@gmail.com>
2024-06-03 18:38:20 -0300

    fix:{adjustments on userSettings layout}

--- src/components/_ui/userSettings/UserSettings.tsx
+++ src/components/_ui/userSettings/UserSettings.tsx
@@ -5,7 +5,7 @@ import { IconEdit } from "@tabler/icons-react";
 export default function UserSettings({ userName, userEmail }: any) {
   return (
     <>
-      <Paper radius="md" withBorder maw='90vw' w='30rem' p="lg" >
+      <Paper radius="md" withBorder maw='90vw' mx='auto' w='30rem' p="lg" >
         <ProviderUser name={userName} size={120} />
         <Text ta="center" fz="lg" fw={500} mt="md">
           {userName}
