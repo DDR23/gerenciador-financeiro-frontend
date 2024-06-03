57293d3453778de2f9a18d8e07e456088fb655af
DDR23 <roboreserva.andre@gmail.com>
2024-06-02 23:31:54 -0300

    fix:{more adjustments}

--- src/components/_ui/userSettings/UserSettings.tsx
+++ src/components/_ui/userSettings/UserSettings.tsx
@@ -1,9 +1,23 @@
-import { Text } from "@mantine/core";
+import { Button, Paper, Text } from "@mantine/core";
+import ProviderUser from "../../../services/ProviderUser";
+import { IconEdit } from "@tabler/icons-react";
 
-export default function UserSettings() {
+export default function UserSettings({ userName, userEmail }: any) {
   return (
     <>
-      <Text inline >User Settings</Text>
+      <Paper radius="md" withBorder maw='90vw' w='30rem' p="lg" >
+        <ProviderUser name={userName} size={120} />
+        <Text ta="center" fz="lg" fw={500} mt="md">
+          {userName}
+        </Text>
+        <Text ta="center" c="dimmed" fz="sm">
+          {userEmail}
+        </Text>
+
+        <Button leftSection={<IconEdit />} variant="default" fullWidth mt="md">
+          Edit User
+        </Button>
+      </Paper>
     </>
   )
 }
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -12,81 +12,79 @@ import { useEffect, useState } from "react";
 import useGet from "../../services/useGet";
 
 export default function Dashboard() {
-  const authToken = localStorage.getItem('token')
-  const { logout } = useAuth();
-
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
   const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+  const { logout } = useAuth();
+  const authToken = localStorage.getItem('token')
 
   const [selectedTab, setSelectedTab] = useState<any>(() => {
-    const storedTab = localStorage.getItem('selectedTab');
-    return storedTab || 'dashboard';
+    return localStorage.getItem('selectedTab') || 'dashboard';
   });
-  
-  const { data } = useGet(`${import.meta.env.VITE_BASE_URL}/me`, {
-    headers: {
-      Authorization: `Bearer ${authToken}`
-    }
-  });
-  
-  const userName = data ? data.USER_NAME: '';
 
   useEffect(() => {
     localStorage.setItem('selectedTab', selectedTab);
   }, [selectedTab]);
 
+  const { data } = useGet(`${import.meta.env.VITE_BASE_URL}/me`, {
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  });
+
+  const userName = data ? data.USER_NAME : '';
+  const userEmail = data ? data.USER_EMAIL : '';
+
+
   return (
-    <>
-      <AppShell
-        header={{ height: 60 }}
-        navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !mobileOpened, desktop: !desktopOpened } }}
-        padding="md"
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !mobileOpened, desktop: !desktopOpened } }}
+      padding="md"
+    >
+      <Tabs
+        value={selectedTab}
+        onChange={setSelectedTab}
+        orientation="vertical"
       >
-        <Tabs
-          value={selectedTab}
-          onChange={setSelectedTab}
-          orientation="vertical"
-        >
-          <AppShell.Header>
-            <Group h="100%" px="md" justify="space-between">
-              <Group>
-                <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-                <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
-                <Image src='coin.png' style={{
-                  width: '2rem'
-                }} />
-              </Group>
-              <Group>
-                <Text c="dimmed" size="sm" inline >Welcome, {userName}</Text>
-                <ProviderUser name={userName} />
-              </Group>
+        <AppShell.Header>
+          <Group h="100%" px="md" justify="space-between">
+            <Group>
+              <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+              <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+              <Image src='coin.png' style={{
+                width: '2rem'
+              }} />
             </Group>
-          </AppShell.Header>
-          <AppShell.Navbar p="md" style={{
-            justifyContent: 'space-between'
-          }}>
-            <Tabs.List>
-              <Tabs.Tab value="dashboard" leftSection={<IconHome />}>Dashboard</Tabs.Tab>
-              <Tabs.Tab value="goal" leftSection={<IconTargetArrow />}>Goals</Tabs.Tab>
-              <Tabs.Tab value="category" leftSection={<IconCategory2 />}>Category</Tabs.Tab>
-              <Tabs.Tab value="transaction" leftSection={<IconSwitchHorizontal />}>Transaction</Tabs.Tab>
-              <Tabs.Tab value="settings" leftSection={<IconSettings />}>Settings</Tabs.Tab>
-            </Tabs.List>
-            <Group >
-              <Button onClick={() => logout()} fullWidth leftSection={<IconLogout />} >
-                Logout
-              </Button>
+            <Group>
+              <Text c="dimmed" size="sm" inline >Welcome, {userName}</Text>
+              <ProviderUser name={userName} size='2.3rem' />  
             </Group>
-          </AppShell.Navbar>
-          <AppShell.Main>
-            <Tabs.Panel value="dashboard"><UserPanel /></Tabs.Panel>
-            <Tabs.Panel value="goal"><UserGoal /></Tabs.Panel>
-            <Tabs.Panel value="category"><UserCategory /></Tabs.Panel>
-            <Tabs.Panel value="transaction"><UserTransaction /></Tabs.Panel>
-            <Tabs.Panel value="settings"><UserSettings /></Tabs.Panel>
-          </AppShell.Main>
-        </Tabs>
-      </AppShell>
-    </>
+          </Group>
+        </AppShell.Header>
+        <AppShell.Navbar p="md" style={{
+          justifyContent: 'space-between'
+        }}>
+          <Tabs.List>
+            <Tabs.Tab value="dashboard" leftSection={<IconHome />}>Dashboard</Tabs.Tab>
+            <Tabs.Tab value="goal" leftSection={<IconTargetArrow />}>Goals</Tabs.Tab>
+            <Tabs.Tab value="category" leftSection={<IconCategory2 />}>Category</Tabs.Tab>
+            <Tabs.Tab value="transaction" leftSection={<IconSwitchHorizontal />}>Transaction</Tabs.Tab>
+            <Tabs.Tab value="settings" leftSection={<IconSettings />}>Settings</Tabs.Tab>
+          </Tabs.List>
+          <Group >
+            <Button onClick={() => logout()} fullWidth leftSection={<IconLogout />} >
+              Logout
+            </Button>
+          </Group>
+        </AppShell.Navbar>
+        <AppShell.Main mx='auto'>
+          <Tabs.Panel value="dashboard"><UserPanel /></Tabs.Panel>
+          <Tabs.Panel value="goal"><UserGoal /></Tabs.Panel>
+          <Tabs.Panel value="category"><UserCategory /></Tabs.Panel>
+          <Tabs.Panel value="transaction"><UserTransaction /></Tabs.Panel>
+          <Tabs.Panel value="settings"><UserSettings userName={userName} userEmail={userEmail}/></Tabs.Panel>
+        </AppShell.Main>
+      </Tabs>
+    </AppShell>
   )
 }
