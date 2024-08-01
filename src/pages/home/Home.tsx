483bd17fa16d80983141009247da942bd3b84a7b
DDR23 <roboreserva.andre@gmail.com>
2024-07-31 21:50:58 -0300

    fix:{css agora é inline}

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,4 @@
-import { Anchor, Button, Group, Image, Modal, Text, Title } from "@mantine/core";
-import "./index.scss"
+import { Anchor, Button, Group, Image, Modal, Stack, Text, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useState } from "react";
 import ModalSignin from "../../components/_ui/modalSignin/ModalSignin";
@@ -16,59 +15,59 @@ export default function Home() {
 
   return (
     <>
-      <div className="home" >
-        <div className="home__content" >
-          <Image src='coin.png' />
+      <Stack w='90vw' h='100vh' align="center" justify="center" gap="sm" m='auto'>
+        <Image src='coin.png' w={200} />
+        <Stack ta='center' align='center' gap={0}>
           <Title order={1}>Manage <Text span c="green" inherit>Your Finance</Text> with Ease</Title>
-          <Title fw={500} order={2}>Effortlessly track your bills, income, and expenses. <Text span c="green" inherit fw='500'>Take control of your money</Text> with simplicity and clarity.</Title>
-          <Group className="home__content--btn">
-            <Button
-              fw={500}
-              onClick={() => handleOpen('login')}
-            >
-              Login
-            </Button>
-            <Button
-              fw={500}
-              onClick={() => handleOpen('signup')}
-              variant="outline"
-            >
-              Sign up
-            </Button>
-          </Group>
-          <Modal
-            opened={opened}
-            onClose={close}
-            withCloseButton={false}
-            overlayProps={{
-              backgroundOpacity: 0.55,
-              blur: 3
-            }}>
-            {modalContent === 'login' && (
-              <>
-                <ModalSignin />
-                <Text c="dimmed" size="sm" ta="center" mt={20}>
-                  Do not have an account yet?{' '}
-                  <Anchor size="sm" component="button" onClick={() => handleOpen('signup')}>
-                    Create account
-                  </Anchor>
-                </Text>
-              </>
-            )}
-            {modalContent === 'signup' && (
-              <>
-                <ModalSingup />
-                <Text c="dimmed" size="sm" ta="center" mt={20}>
-                  Already have an account?{' '}
-                  <Anchor size="sm" component="button" onClick={() => handleOpen('login')}>
-                    Login
-                  </Anchor>
-                </Text>
-              </>
-            )}
-          </Modal>
-        </div>
-      </div>
+          <Title fw={500} order={4}>Effortlessly track your bills, income, and expenses. <Text span c="green" inherit fw='500'>Take control of your money</Text> with simplicity and clarity.</Title>
+        </Stack>
+        <Group className="home__content--btn">
+          <Button
+            fw={500}
+            onClick={() => handleOpen('login')}
+          >
+            Login
+          </Button>
+          <Button
+            fw={500}
+            onClick={() => handleOpen('signup')}
+            variant="outline"
+          >
+            Sign up
+          </Button>
+        </Group>
+        <Modal
+          opened={opened}
+          onClose={close}
+          withCloseButton={false}
+          overlayProps={{
+            backgroundOpacity: 0.55,
+            blur: 3
+          }}>
+          {modalContent === 'login' && (
+            <>
+              <ModalSignin />
+              <Text c="dimmed" size="sm" ta="center" mt={20}>
+                Do not have an account yet?{' '}
+                <Anchor size="sm" component="button" onClick={() => handleOpen('signup')}>
+                  Create account
+                </Anchor>
+              </Text>
+            </>
+          )}
+          {modalContent === 'signup' && (
+            <>
+              <ModalSingup />
+              <Text c="dimmed" size="sm" ta="center" mt={20}>
+                Already have an account?{' '}
+                <Anchor size="sm" component="button" onClick={() => handleOpen('login')}>
+                  Login
+                </Anchor>
+              </Text>
+            </>
+          )}
+        </Modal>
+      </Stack>
     </>
   );
 }
